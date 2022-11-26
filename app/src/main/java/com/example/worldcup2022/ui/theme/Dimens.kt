9d51037080a6e1eff904a33ab12ca258be1720cb
ameeramjed <pr.ameer98@gmail.com>
2022-11-26 22:30:50 +0300

    add font dimens

---
+++ app/src/main/java/com/example/worldcup2022/ui/theme/Dimens.kt
@@ -0,0 +1,24 @@
+package com.example.worldcup2022.ui.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+val zero: Dp = 0.dp
+
+val spacing: Dp = 4.dp
+val spacingSmall: Dp = 8.dp
+val spacingMedium: Dp = 16.dp
+val spacingLarge: Dp = 24.dp
+val spacingExtraLarge: Dp = 32.dp
+val spacingHuge: Dp = 48.dp
+
+val fontSizePrimary: TextUnit = 16.sp
+val fontSizesSecondary: TextUnit = 18.sp
+val fontSizesCaption: TextUnit = 12.sp
+val fontSizesTitle: TextUnit = 24.sp
+val fontSizesTitleExtra: TextUnit = 32.sp
+
+val avatarSmall: Dp = 50.dp
